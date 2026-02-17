38a0be03723233c3ab99f020c9fbfea7a41c5975
Zarifjon Baxtiyorov <baxtiyorovzarif@gmail.com>
2026-02-17 13:00:23 +0500

    change debug ture from false

--- config/config/settings.py
+++ config/config/settings.py
@@ -15,7 +15,9 @@ import os
 import dj_database_url
 from decouple import config
 
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY", default="dev-secret")
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,11 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)vk3piv0ob%$p2(7f-l98!g%%%x^e^y9)=l=^)f*+#%#@2$fy$'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*", cast=lambda v: v.split(","))
 
